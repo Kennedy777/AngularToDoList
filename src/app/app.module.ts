ae538755771b6f96e3d1f2defcc44508822d023f
Guest User <Guest@epicodus-79.local>
2019-06-12 13:55:50 -0700

    add to do list created in Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,24 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-
+import { FormsModule } from '@angular/forms';
+import { TaskListComponent } from './task-list/task-list.component';
+import { EditTaskComponent } from './edit-task/edit-task.component';
+import { NewTaskComponent } from './new-task/new-task.component';
+import { CompletenessPipe } from './completeness.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
+    AppComponent,
+    TaskListComponent,
+    EditTaskComponent,
+    NewTaskComponent,
+    CompletenessPipe ],
+    imports: [
+   BrowserModule,
+   FormsModule
+ ],
+ providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
